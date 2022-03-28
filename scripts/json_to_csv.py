8c7c5de064ac2e40848f7c35ef0ec8ef796f8922
vzhang88 <vzha9726@uni.sydney.edu.au>
2022-03-28 20:29:56 +1100

    Commented json_to_csv.py

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -3,11 +3,14 @@ import os
 os.chdir(r"C:\Users\vince\Documents\thesis\data\raw_data")
 cwd = os.getcwd()
          
+# Creates a CSV version of the given JSON file 
 def convert_file(file):
     with open(file, encoding='utf-8') as inputfile:
         df = pd.read_json(inputfile)
         df.to_csv(file.replace('json','csv'), encoding='utf-8', index=False)
+        
             
+# Creates a CSV file for each JSON file in the current working directory 
 def replace_json_with_csv(cwd):
     num_files = 0
     for root, dirs, files in os.walk(cwd):
@@ -18,6 +21,8 @@ def replace_json_with_csv(cwd):
                 num_files += 1 
     print("The total number of files is: {}".format(num_files))
 
+
+# Deletes all JSON files within the current working directory 
 def delete_json_files(cwd):
     num_files = 0
     for root, dirs, files in os.walk(cwd):
@@ -27,12 +32,15 @@ def delete_json_files(cwd):
                 num_files += 1 
     print("The total number of JSON files deleted is: {}".format(num_files))
 
+
+# Counts the total number of files within the director 
 def count_num_files(cwd):
     num_files = 0
     for root, dirs, files in os.walk(cwd):
         for file in files:
             num_files += 1
-    print("The total number of JSON files deleted is: {}".format(num_files))
+    print("The total number of files is: {}".format(num_files))
+
 
 replace_json_with_csv(cwd)
 delete_json_files(cwd)
