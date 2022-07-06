603c31456de540c3fd346ee625870b2f7c31f37e
vzhang88 <vzha9726@uni.sydney.edu.au>
2022-07-06 22:12:44 +1000

    accurate slide completion

--- scripts/slide_completion.py
+++ scripts/slide_completion.py
@@ -1,10 +1,13 @@
 from matplotlib import interactive
 import pandas as pd 
 import os
-from dask import dataframe as dd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+
+# Key changes: We are only keeping one record of the interaction string for each student
+# We are not holding multiple records that show the student's progression / slide completion order
+# This will be captured by another type of encoding 
 
 from sklearn.metrics import jaccard_score
 os.chdir(r"C:\Users\vince\Documents\thesis\data\raw_data\events")
@@ -19,58 +22,68 @@ def main():
     file = 'events_processed.csv'
     df = filter(file)
     second_df = df[(df.slide_no < 5)]
+
+    # Sort the dataset by users 
     second_df.sort_values(by=['user_id'])
     student_dict = {}
-    
-    student = "3b9f1dbe452304fce7316bbb9734b7d3"
-    student_dict[student] = []
-    interaction_string = [0]*5
-    previous_slide = None
 
+    # Check the number of students in the dataset 
+    print("The number of students is {}".format(len(second_df["user_id"].unique())))
+    
+    # The ID of the first student 
+    student = "3b9f1dbe452304fce7316bbb9734b7d3"
+    
+    # The interaction string for each student 
+    interaction_string = [0]*6
+
+    student_dict[student] = interaction_string
+
+    # Iterate through the rows of the dataframe
     for row in second_df.itertuples():
+
+        # We have encountered a new student
         if row.user_id != student:
-            if previous_slide == "problem":
-                student_dict[student].append(interaction_string)
-                interaction_string = [0]*5
+
+            # Assign the current student's ID to the student variable
             student = row.user_id
-            student_dict[student] = []
-            interaction_string = [0]*5
-             
+
+            # Construct a new zeroed interaction string and assign
+            # it to the corresponding student entry in the dict
+            interaction_string = [0]*6
+            student_dict[student] = interaction_string
+
+        # We have encountered a slide steps complete event
         if row.event_name == "slide_steps_complete":
-            interaction_string[row.slide_no] = 1
-            if previous_slide == "problem":
-                student_dict[student].append(interaction_string)
-                interaction_string = [0]*5
-            previous_slide = "interactive"
-             
+            student_dict[student][row.slide_no] = 1
+
         elif row.event_name == "problem_failed":
-            interaction_string[row.slide_no] = 1
-            previous_slide = "problem"
+            student_dict[student][row.slide_no] = 1
 
         elif row.event_name == "problem_passed":
-            interaction_string[row.slide_no] = 2
-            previous_slide = "problem"
+            student_dict[student][row.slide_no] = 2
+
 
     final_student_dict = {}
     for student in student_dict:
-        if len(student_dict[student]) != 0 and [0,0,0,0,0] not in student_dict[student]:
+        if len(student_dict[student]) != 0:
+            # and [0,0,0,0,0,0] not in student_dict[student]
             final_student_dict[student] = student_dict[student]
 
     outcomes_dict = {}
     sequences_dict = {}
     for student in final_student_dict:
-        for entry in final_student_dict[student]:
-            sequence = tuple(entry)
-            if sequence not in sequences_dict:
-                sequences_dict[sequence] = 0
-            else:
-                sequences_dict[sequence] += 1
+        entry = final_student_dict[student] # Adds all the runs
+        sequence = tuple(entry)
+        if sequence not in sequences_dict:
+            sequences_dict[sequence] = 1
+        else:
+            sequences_dict[sequence] += 1
 
-            outcome = tuple(entry[-2:])
-            if outcome not in outcomes_dict:
-                outcomes_dict[outcome] = 0
-            else:
-                outcomes_dict[outcome] += 1
+        outcome = tuple(entry[-2:]) # initially we had 5 spots, don't count slide 0
+        if outcome not in outcomes_dict:
+            outcomes_dict[outcome] = 1
+        else:
+            outcomes_dict[outcome] += 1
 
             # Tuples are hashable 
 
