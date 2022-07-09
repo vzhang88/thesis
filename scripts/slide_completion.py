79bc35af4f1d5c457cf9679887f114f9b91f8ad0
vzhang88 <vzha9726@uni.sydney.edu.au>
2022-07-09 21:51:23 +1000

    Modulated slide completion

--- scripts/slide_completion.py
+++ scripts/slide_completion.py
@@ -22,39 +22,82 @@ def filter(file):
     filtered_df = df[(df.challenge_name == "challenge-newbies-2018") & (df.problem_name == "w1p1")] 
     return filtered_df
 
-def main():
-    file = 'events_processed.csv'
-    df = filter(file)
+def plot_results(final_outcomes_dict, final_sequences_dict):
+    plt.bar([str(key) for key in final_outcomes_dict.keys()], final_outcomes_dict.values())
+    plt.title("Distribution of problem outcomes")
+    plt.xlabel("Outcomes")
+    plt.ylabel("Count")
+    plt.show()
 
-    # The number of slides in the module (+1 to account for 0-indexing)
-    num_slides = df["slide_no"].max() + 1  
-    print("The number of slides in the module is {}".format(num_slides))
-
-    # Problem slides containing the slide numbers of the designated problems 
-    problem_slides = []
-    problem_df = df[(df.event_name == "problem_passed")] # This is a useful check 
-    for slide_no in problem_df["slide_no"].unique():
-
-        # Note: Some interactive slides are mislabelled as problem slides. To account
-        # for this, take the problem slides that have a high rate of visits from users
-        if len(problem_df[(problem_df.slide_no == slide_no)]) > 500:
-            problem_slides.append(slide_no)
-
-    problem_slides.sort()
-    print("The problem slides are {}".format(problem_slides))
-
-    # Sanity check to check how many people completed a particular slide
-    problem_df = df[(df.event_name == "problem_passed") & (df.slide_no == problem_slides[0])] # useful check
-    print("The number of students which completed the first problem slide is {}".format(len(problem_df["user_id"].unique())))
-
-    # Sort the dataset by users 
-    df = df.sort_values(by=['user_id']) # CAREFUL
-    student_dict = {}
-
-    # Check the number of students in the dataset 
-    num_students = len(df["user_id"].unique())
-    print("The number of students is {}".format(num_students))
+    plt.bar([str(key) for key in final_sequences_dict.keys()], final_sequences_dict.values())
+    plt.title("Distribution of interaction sequences")
+    plt.xlabel("Interaction sequence")
+    plt.ylabel("Count")
+    plt.show()
     
+# Aggregate all outcomes that do not appear as frequently
+# into a separate category called "Other"
+def aggregate(outcomes_dict, sequences_dict, num_students):
+    final_outcomes_dict = {}
+    for outcome in outcomes_dict:
+        if outcomes_dict[outcome] < num_students/10:
+            if "Other" not in final_outcomes_dict:
+                final_outcomes_dict["Other"] = outcomes_dict[outcome]
+            else:
+                final_outcomes_dict["Other"] += outcomes_dict[outcome]
+        else:
+            final_outcomes_dict[outcome] = outcomes_dict[outcome]
+
+    # Aggregate all sequences that do not appear as frequently
+    # into a separate category called "Other"
+    final_sequences_dict = {}
+    for sequence in sequences_dict:
+        if sequences_dict[sequence] < num_students/10:
+            if "Other" not in final_sequences_dict:
+                final_sequences_dict["Other"] = sequences_dict[sequence]
+            else:
+                final_sequences_dict["Other"] += sequences_dict[sequence]
+        else:
+            final_sequences_dict[sequence] = sequences_dict[sequence]
+
+    return final_outcomes_dict, final_sequences_dict
+
+
+def get_results_dicts(student_dict, problem_slides):
+    # Dictionary to store the outcome tuples and their 
+    # corresponding counts 
+    outcomes_dict = {}
+
+    # Dictionary to store the interaction sequences and their 
+    # corresponding counts 
+    sequences_dict = {}
+
+    for student in student_dict:
+        entry = student_dict[student] 
+        sequence = tuple(entry)
+        if sequence not in sequences_dict:
+            sequences_dict[sequence] = 1
+        else:
+            sequences_dict[sequence] += 1
+        
+        # Indices of the first pair of problem slides 
+        idx0 = problem_slides[0]
+        idx1 = problem_slides[1]
+        
+        # Indices of the second pair of problem slides 
+        idx2 = problem_slides[2]
+        idx3 = problem_slides[3]
+
+        outcome = tuple(entry[idx0:idx1+1] + entry[idx2:idx3+1]) 
+        if outcome not in outcomes_dict:
+            outcomes_dict[outcome] = 1
+        else:
+            outcomes_dict[outcome] += 1
+    
+    return outcomes_dict, sequences_dict
+
+def get_student_dict(df, num_slides, problem_slides):
+    student_dict = {}
     # The ID of the first student 
     student = df.iloc[0].user_id
 
@@ -96,79 +139,55 @@ def main():
         elif row.event_name == "problem_passed" and row.slide_no in problem_slides:
             student_dict[student][row.slide_no] = "P" 
 
-    # Dictionary to store the outcome tuples and their 
-    # corresponding counts 
-    outcomes_dict = {}
+    return student_dict
 
-    # Dictionary to store the interaction sequences and their 
-    # corresponding counts 
-    sequences_dict = {}
+def get_problem_slides(df):
+    # Problem slides containing the slide numbers of the designated problems 
+    problem_slides = []
+    problem_df = df[(df.event_name == "problem_passed")] # This is a useful check 
+    for slide_no in problem_df["slide_no"].unique():
 
-    for student in student_dict:
-        entry = student_dict[student] 
-        sequence = tuple(entry)
-        if sequence not in sequences_dict:
-            sequences_dict[sequence] = 1
-        else:
-            sequences_dict[sequence] += 1
-        
-        # Indices of the first pair of problem slides 
-        idx0 = problem_slides[0]
-        idx1 = problem_slides[1]
-        
-        # Indices of the second pair of problem slides 
-        idx2 = problem_slides[2]
-        idx3 = problem_slides[3]
+        # Note: Some interactive slides are mislabelled as problem slides. To account
+        # for this, take the problem slides that have a high rate of visits from users
+        if len(problem_df[(problem_df.slide_no == slide_no)]) > 500:
+            problem_slides.append(slide_no)
+    problem_slides.sort()
+    print("The problem slides are {}".format(problem_slides))
+    return problem_slides
 
-        outcome = tuple(entry[idx0:idx1+1] + entry[idx2:idx3+1]) 
-        if outcome not in outcomes_dict:
-            outcomes_dict[outcome] = 1
-        else:
-            outcomes_dict[outcome] += 1
 
-    # Aggregate all outcomes that do not appear as frequently
-    # into a separate category called "Other"
-    final_outcomes_dict = {}
-    for outcome in outcomes_dict:
-        if outcomes_dict[outcome] < num_students/10:
-            if "Other" not in final_outcomes_dict:
-                final_outcomes_dict["Other"] = outcomes_dict[outcome]
-            else:
-                final_outcomes_dict["Other"] += outcomes_dict[outcome]
-        else:
-            final_outcomes_dict[outcome] = outcomes_dict[outcome]
+def main():
+    file = 'events_processed.csv'
+    df = filter(file)
 
-    # Aggregate all sequences that do not appear as frequently
-    # into a separate category called "Other"
-    final_sequences_dict = {}
-    for sequence in sequences_dict:
-        if sequences_dict[sequence] < num_students/10:
-            if "Other" not in final_sequences_dict:
-                final_sequences_dict["Other"] = sequences_dict[sequence]
-            else:
-                final_sequences_dict["Other"] += sequences_dict[sequence]
-        else:
-            final_sequences_dict[sequence] = sequences_dict[sequence]
+    # The number of slides in the module (+1 to account for 0-indexing)
+    num_slides = df["slide_no"].max() + 1  
+    print("The number of slides in the module is {}".format(num_slides))
 
-    plt.bar([str(key) for key in final_outcomes_dict.keys()], final_outcomes_dict.values())
-    plt.title("Distribution of problem outcomes")
-    plt.xlabel("Outcomes")
-    plt.ylabel("Count")
-    plt.show()
+    problem_slides = get_problem_slides(df)
 
-    plt.bar([str(key) for key in final_sequences_dict.keys()], final_sequences_dict.values())
-    plt.title("Distribution of interaction sequences")
-    plt.xlabel("Interaction sequence")
-    plt.ylabel("Count")
-    plt.show()
+    # Sanity check to check how many people completed a particular slide
+    problem_df = df[(df.event_name == "problem_passed") & (df.slide_no == problem_slides[0])] # useful check
+    print("The number of students which completed the first problem slide is {}".format(len(problem_df["user_id"].unique())))
+
+    # Sort the dataset by users 
+    df = df.sort_values(by=['user_id'])  
+    student_dict = {}
+
+    # Check the number of students in the dataset 
+    num_students = len(df["user_id"].unique())
+    print("The number of students is {}".format(num_students))
+    
+    student_dict = get_student_dict(df, num_slides, problem_slides)
+    outcomes_dict, sequences_dict = get_results_dicts(student_dict, problem_slides)
+    final_outcomes_dict, final_sequences_dict = aggregate(outcomes_dict, sequences_dict, num_students)
+    plot_results(final_outcomes_dict, final_sequences_dict)
     
 if __name__ == "__main__":
     main()
 
+ 
 
-
-
-
-
+ 
 
  
