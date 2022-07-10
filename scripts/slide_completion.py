927139b5a2a857a518357eb110accf8f0e62be1b
vzhang88 <vzha9726@uni.sydney.edu.au>
2022-07-10 10:40:37 +1000

    added sim file

--- scripts/slide_completion.py
+++ scripts/slide_completion.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 
 # Key changes
+# In addition to the below, moved the simulations to a separate file 
+# called slide_completion_sim.py. This file does not have a main function.
 # 1) Fixed a major (I wasn't actually sorting the slides previously), so now
 # the distributions are accurate
 # 2) In addition, aggregated less frequently occuring outcomes 
@@ -155,38 +157,6 @@ def get_problem_slides(df):
     print("The problem slides are {}".format(problem_slides))
     return problem_slides
 
-
-def main():
-    file = 'events_processed.csv'
-    df = filter(file)
-
-    # The number of slides in the module (+1 to account for 0-indexing)
-    num_slides = df["slide_no"].max() + 1  
-    print("The number of slides in the module is {}".format(num_slides))
-
-    problem_slides = get_problem_slides(df)
-
-    # Sanity check to check how many people completed a particular slide
-    problem_df = df[(df.event_name == "problem_passed") & (df.slide_no == problem_slides[0])] # useful check
-    print("The number of students which completed the first problem slide is {}".format(len(problem_df["user_id"].unique())))
-
-    # Sort the dataset by users 
-    df = df.sort_values(by=['user_id'])  
-    student_dict = {}
-
-    # Check the number of students in the dataset 
-    num_students = len(df["user_id"].unique())
-    print("The number of students is {}".format(num_students))
-    
-    student_dict = get_student_dict(df, num_slides, problem_slides)
-    outcomes_dict, sequences_dict = get_results_dicts(student_dict, problem_slides)
-    final_outcomes_dict, final_sequences_dict = aggregate(outcomes_dict, sequences_dict, num_students)
-    plot_results(final_outcomes_dict, final_sequences_dict)
-    
-if __name__ == "__main__":
-    main()
-
- 
 
  
 
